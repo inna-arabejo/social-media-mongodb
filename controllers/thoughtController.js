4596f47a9bfec482720b50bfd60b59202221cb61
Anne Saber <iarabejo3@gmail.com>
2022-04-27 22:36:26 -0500

    Get all, single, create, and update thought in thoughtControllers.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,7 +1,7 @@
 const { User, Thought } = require('../models');
 
 const thoughtController = {
-  getThoughts( req, res ){
+  getThoughts(req, res) {
     Thought.find()
     .sort({
       createdAt: -1
@@ -15,7 +15,7 @@ const thoughtController = {
   },
 
   // getSingleThought by id
-  getSingleThought( req, res ){
+  getSingleThought(req, res) {
     Thought.findOne({_id: req.params.id})
     .sort({
       createdAt: -1
@@ -28,10 +28,48 @@ const thoughtController = {
     });
   },
 
+  createThought({ params, body }, res) {
+    Thought.create(body)
+    .then(thought => {
+      User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { thoughts: thought._id } },
+        { new: true },
+      )
+      .then(dbThoughtData => {
+        if (!dbThoughtData){
+          res.status(404).json({ message: 'No thought found with this id'});
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+    });
+  },
+
+  updateThought({ params, body }, res) {
+    Thought.findByIdAndUpdate(
+      { _id: params.thoughtId },
+      body,
+      { runValidators: true, new: true }
+    )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought with this id'});
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch((err) => {
+      res.json(err)
+    });
+  },
+
+
 }
 
-// createThought
-// updateThought
 // deleteThought
 // addReactionToAThought
 // removeReactionFromAThought
